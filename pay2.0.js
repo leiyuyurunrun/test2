bc692b999adb9f30cea94c6515404f30bfbff0d5
leiyuyurunrun <116471071+leiyuyurunrun@users.noreply.github.com>
2023-03-02 09:12:49 +0800

    Update pay2.0.js

--- pay2.0.js
+++ pay2.0.js
@@ -4,14 +4,14 @@ setScreenMetrics(1080, 2240);
 home();
 launchApp("支付宝");
 sleep(2000);
-console.show();
-log("好戏就要开场了，客官稍等……");
+
+toast("好戏就要开场了，客官稍等……");
 
 var farm = text('芭芭农场').findOne();
 var click_true = click(farm.bounds().centerX(), farm.bounds().centerY());
-log("尊敬的任小风，任先锋，绝yi，不找对象不换头像的人，头像像中老年的人inneace，human without balls, 憋屎第一人，美而不自知的敏妹，你好。");
+toast("尊敬的任小风，任先锋，绝yi，不找对象不换头像的人，头像像中老年的人inneace，human without balls, 憋屎第一人，美而不自知的敏妹，你好。");
 sleep(4000);
-log("让我们来到第一项服务，广告服务；");
+toast("让我们来到第一项服务，广告服务；");
 var task=text("任务列表").depth(15).findOne();
 task.click();
 sleep(1500);
@@ -38,7 +38,7 @@ sleep(1000);
      }
 click(500, 400);
 
-log("电视服务结束了，该起床浇树了；");
+toast("电视服务结束了，该起床浇树了；");
 var i = 0;
 while(i<timesInOne){
   i++;
@@ -46,4 +46,4 @@ while(i<timesInOne){
   sleep(800);
  
  }
-log("本次服务到此结束，请自觉打赏leiyuyu。");
+toast("本次服务到此结束，请自觉打赏leiyuyu。");
